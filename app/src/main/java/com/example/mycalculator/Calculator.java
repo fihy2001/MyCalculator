8ed97f95460b9e6b3c9f84f4568eb2e44d7fccf8
James <fihy2001@hotmail.com>
2020-07-21 12:23:44 -0400

    Changes up to equal and clear

--- app/src/main/java/com/example/mycalculator/Calculator.java
+++ app/src/main/java/com/example/mycalculator/Calculator.java
@@ -9,12 +9,29 @@ import android.widget.TextView;
 
 public class Calculator extends Activity implements View.OnClickListener {
 
+    private enum OPERATOR {
+        PLUS, SUBTRACT, MULTIPLY, DIVIDE, EQUAL
+    }
     TextView txtCalculations, txtResults;
+
+    // Instance Variables
+
+    private String currentNumber;
+    private String stringNumberAtLeft;
+    private String stringNumberAtRight;
+    private OPERATOR currentOperator;
+    private int calculationsResult;
+
+    private String calculationsString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        currentNumber = "";
+        calculationsResult = 0;
+        calculationsString = "";
 
         txtCalculations = findViewById(R.id.txtCalculations);
         txtResults = findViewById(R.id.txtResults);
@@ -47,51 +64,114 @@ public class Calculator extends Activity implements View.OnClickListener {
                 break;
 
             case R.id.btn0:
+                numberIsTapped(0);
                 break;
 
             case R.id.btn1:
+                numberIsTapped(1);
                 break;
 
             case R.id.btn2:
+                numberIsTapped(2);
                 break;
 
             case R.id.btn3:
+                numberIsTapped(3);
                 break;
 
             case R.id.btn4:
+                numberIsTapped(4);
                 break;
 
             case R.id.btn5:
+                numberIsTapped(5);
                 break;
 
             case R.id.btn6:
+                numberIsTapped(6);
                 break;
 
             case R.id.btn7:
+                numberIsTapped(7);
                 break;
 
             case R.id.btn8:
+                numberIsTapped(8);
                 break;
 
             case R.id.btn9:
+                numberIsTapped(9);
                 break;
 
             case R.id.btnClear:
                 break;
 
             case R.id.btnPlus:
+                operatorIsTapped(OPERATOR.PLUS);
+                calculationsString += " + ";
+                //calculationsString = calculationsString + " + ";
                 break;
 
             case R.id.btnSubtract:
+                operatorIsTapped(OPERATOR.SUBTRACT);
+                calculationsString += " - ";
                 break;
 
             case R.id.btnMultiply:
+                operatorIsTapped(OPERATOR.MULTIPLY);
+                calculationsString += " * ";
                 break;
 
             case R.id.btnDivide:
+                operatorIsTapped(OPERATOR.DIVIDE);
+                calculationsString += " / ";
                 break;
 
         }
+        txtCalculations.setText(calculationsString);
 
+    }
+
+    private void numberIsTapped(int tappedNumber){
+        currentNumber = currentNumber + tappedNumber;
+        txtResults.setText(currentNumber);
+        calculationsString = currentNumber;
+        txtCalculations.setText(calculationsString);
+    }
+
+    private void operatorIsTapped(OPERATOR tappedOperator){
+        if(currentOperator != null && currentNumber != ""){
+            stringNumberAtRight = currentNumber;
+            currentNumber = "";
+
+            switch (currentOperator){
+                case PLUS:
+                    calculationsResult = Integer.parseInt(stringNumberAtLeft) +
+                            Integer.parseInt(stringNumberAtRight);
+                    break;
+                case SUBTRACT:
+                    calculationsResult = Integer.parseInt(stringNumberAtLeft) -
+                            Integer.parseInt(stringNumberAtRight);
+                    break;
+                case MULTIPLY:
+                    calculationsResult = Integer.parseInt(stringNumberAtLeft) *
+                            Integer.parseInt(stringNumberAtRight);
+                    break;
+                case DIVIDE:
+                    calculationsResult = Integer.parseInt(stringNumberAtLeft) /
+                            Integer.parseInt(stringNumberAtRight);
+                    break;
+                case EQUAL:
+                    break;
+            }
+            stringNumberAtLeft = String.valueOf(calculationsResult);
+            txtResults.setText(stringNumberAtLeft);
+            calculationsString = stringNumberAtLeft;
+
+        } else {
+            stringNumberAtLeft = currentNumber;
+            currentNumber = "";
+        }
+        currentOperator = tappedOperator;
     }
 }
